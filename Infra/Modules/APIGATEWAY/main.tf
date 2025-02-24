b112096d945ff0d1d5d7b07adcaafc851362be4f
Sudharshan12313 <167956857+Sudharshan12313@users.noreply.github.com>
2025-02-24 11:06:34 +0530

    Update main.tf

--- Infra/Modules/APIGATEWAY/main.tf
+++ Infra/Modules/APIGATEWAY/main.tf
@@ -1,106 +1,3 @@
-# Create a REST API Gateway
-resource "aws_api_gateway_rest_api" "greeting_api" {
-  name        = var.api_name
-  description = "API for invoking the Greeting Lambda Function"
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
-
-# Create an API Resource
-resource "aws_api_gateway_resource" "greet_resource" {
-  rest_api_id = aws_api_gateway_rest_api.greeting_api.id
-  parent_id   = aws_api_gateway_rest_api.greeting_api.root_resource_id
-  path_part   = "greet"
-}
-
-# Create an API Method
-resource "aws_api_gateway_method" "greet_method" {
-  rest_api_id   = aws_api_gateway_rest_api.greeting_api.id
-  resource_id   = aws_api_gateway_resource.greet_resource.id
-  http_method   = "POST"
-  authorization = "NONE"
-}
-
-# Grant API Gateway permissions to invoke the Lambda function
-resource "aws_lambda_permission" "allow_api_gateway" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = var.lambda_function_name
-  principal     = "apigateway.amazonaws.com"
-
-  # Restricting access only to this API Gateway
-  source_arn = "${aws_api_gateway_rest_api.greeting_api.execution_arn}/*/${aws_api_gateway_method.greet_method.http_method}${aws_api_gateway_resource.greet_resource.path}"
-}
-
-# Integrate API Gateway with the Lambda function
-resource "aws_api_gateway_integration" "greet_lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.greeting_api.id
-  resource_id = aws_api_gateway_resource.greet_resource.id
-  http_method = aws_api_gateway_method.greet_method.http_method
-
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = "arn:aws:apigateway:${data.aws_region.current.name}:lambda:path/2015-03-31/functions/${var.lambda_function_arn}/invocations"
-
-  depends_on = [aws_lambda_permission.allow_api_gateway]
-}
-
-# Fetch the AWS region dynamically
-data "aws_region" "current" {}
-
-
-/*# Create a new API Gateway deployment
-resource "aws_api_gateway_deployment" "greeting_api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.greeting_api.id
-  stage_name  = var.stage_name
-  tracing_enabled = true  # Enable X-Ray tracing
-
-  triggers = {
-    redeployment = sha256(jsonencode(aws_api_gateway_rest_api.greeting_api.body))
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  depends_on = [
-    aws_api_gateway_method.greet_method,
-    aws_api_gateway_integration.greet_lambda_integration
-  ]
-}*/
-
-
-resource "aws_api_gateway_deployment" "greeting_api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.greeting_api.id
-
-  triggers = {
-    redeployment = sha256(jsonencode(aws_api_gateway_rest_api.greeting_api.body))
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  depends_on = [
-    aws_api_gateway_method.greet_method,
-    aws_api_gateway_integration.greet_lambda_integration
-  ]
-}
-
-resource "aws_api_gateway_stage" "greeting_api_stage" {
-  stage_name    = var.stage_name
-  rest_api_id   = aws_api_gateway_rest_api.greeting_api.id
-  deployment_id = aws_api_gateway_deployment.greeting_api_deployment.id
-}
-
-
-
-resource "aws_sqs_queue" "greeting_queue" {
-  name                    = "greetings_queue"
-  sqs_managed_sse_enabled = true
-}
-
 # Create an IAM Role for API Gateway
 resource "aws_iam_role" "api_gateway_greeting_queue_role" {
   name = "api_gateway_greeting_queue_role"
@@ -136,6 +33,42 @@ resource "aws_iam_role_policy" "api_gateway_greeting_queue_role_policy" {
     ]
   })
 }
+
+
+# Create a REST API Gateway
+resource "aws_api_gateway_rest_api" "greeting_api" {
+  name        = var.api_name
+  description = "API for invoking the Greeting Lambda Function"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+# Create an API Resource
+resource "aws_api_gateway_resource" "greet_resource" {
+  rest_api_id = aws_api_gateway_rest_api.greeting_api.id
+  parent_id   = aws_api_gateway_rest_api.greeting_api.root_resource_id
+  path_part   = "greet"
+}
+
+# Create an API Method
+resource "aws_api_gateway_method" "greet_method" {
+  rest_api_id   = aws_api_gateway_rest_api.greeting_api.id
+  resource_id   = aws_api_gateway_resource.greet_resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+
+# Fetch the AWS region dynamically
+data "aws_region" "current" {}
+
+
+resource "aws_sqs_queue" "greeting_queue" {
+  name                    = "greetings_queue"
+  sqs_managed_sse_enabled = true
+}
+
 
 data "aws_caller_identity" "current" {}
 
@@ -181,29 +114,6 @@ resource "aws_api_gateway_method_response" "method_response_200" {
   }
 }
 
-# Create a policy with permissions required to poll messages from an SQS queue
-resource "aws_iam_policy" "greeting_lambda_sqs_policy" {
-  name        = "greeting_lambda_ssqs_policy"
-  description = "Grants access to read messages from SQS"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = ["sqs:ReceiveMessage", "sqs:DeleteMEssage", "sqs:GetQueueAttributes"],
-        Effect = "Allow",
-        Resource = [aws_sqs_queue.greeting_queue.arn]
-      }
-    ]
-  })
-}
-
-# Attach the policy to Lambda execution role created previously
-resource "aws_iam_role_policy_attachment" "greeting_lambda_sqs_policy_attachment" {
-  policy_arn = aws_iam_policy.greeting_lambda_sqs_policy.arn
-  role       = var.lambda_execution_role_name
-}
-
 # Create a Lambda event-source mapping to enable Lambda to poll from the queue
 resource "aws_lambda_event_source_mapping" "greeting_sqs_mapping" {
   event_source_arn = aws_sqs_queue.greeting_queue.arn
@@ -211,4 +121,23 @@ resource "aws_lambda_event_source_mapping" "greeting_sqs_mapping" {
   batch_size       = 1
 
   depends_on = [aws_iam_role_policy_attachment.greeting_lambda_sqs_policy_attachment ]
+}
+
+# Create a new API Gateway deployment
+resource "aws_api_gateway_deployment" "greeting_api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.greeting_api.id
+  stage_name  = var.stage_name
+
+  triggers = {
+    redeployment = sha256(jsonencode(aws_api_gateway_rest_api.greeting_api.body))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [
+    aws_api_gateway_method.greet_method,
+    aws_api_gateway_integration.greet_lambda_integration
+  ]
 }
